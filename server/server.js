246a804c4c845390bdc89f524e9f1b27f964a050
Stanley Phu <stanley.phu@appfolio.com>
2018-09-21 12:13:50 -0700

    Add socket.io library to app

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const clientPath = path.join(__dirname, '../client');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(clientPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
